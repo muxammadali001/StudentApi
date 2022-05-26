27d38cff03e98870912ce6d1465fb1c7602fe3bf
Muxammad_Ali_21 <91970500+muxammadali001@users.noreply.github.com>
2022-05-26 16:00:06 +0500

    all

--- Program.cs
+++ Program.cs
@@ -1,15 +1,15 @@
+using StudentApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<StudentService>();
+builder.Services.AddSingleton<TeacherService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
